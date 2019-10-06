5572ecbc658ccb5a4f18ad2b5b7623f3da52a5da
Ziad Abdelkarim <zabdelkarim1@gmail.com>
2019-10-06 16:18:50 -0700

    Initial Commit

---
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,56 @@
+/* Ziad Abdelkarim
+ * CSE360 Wed 1030
+ * Assignment 2
+ * This class is intended to hold an int variable and manipulate it through addition or 
+ * subtraction via the underlying methods.
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/*
+	 * Class Constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * Returns the total in variable 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 *  Adds the passed parameter to the total variable 
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * Subtracts the passed parameter from the total variable
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * Returns the total variable as a string
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/*
+	 * Changes the value of the total variable to 0
+	 */
+	public void clear() {
+		
+	}
+	
+}
