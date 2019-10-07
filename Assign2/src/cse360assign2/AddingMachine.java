a9ca4dac68d4a90d769a4c5aa5b995e9a6067199
Ziad Abdelkarim <31060996+ZiadAbdelkarim@users.noreply.github.com>
2019-10-06 22:36:37 -0700

    Changed implementations of AddingMachine class & added JUnit tests

--- Assign2/src/cse360assign2/AddingMachine.java
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -10,47 +10,52 @@ package cse360assign2;
 public class AddingMachine {
 	
 	private int total;
+	private String history;
 	
 	/*
 	 * Class Constructor
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0"; 
 	}
 	
 	/*
-	 * Returns the total in variable 
+	 * Returns the total int variable 
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/*
 	 *  Adds the passed parameter to the total variable 
 	 */
 	public void add (int value) {
-		
+		total += value; 
+		history += " + " + value;
 	}
 	
 	/*
 	 * Subtracts the passed parameter from the total variable
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
 	
 	/*
-	 * Returns the total variable as a string
+	 * Returns the total variable as a string of all manipulations to the original value 0
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 	
 	/*
 	 * Changes the value of the total variable to 0
 	 */
 	public void clear() {
-		
+		total = 0;
+		history = "0"; 
 	}
 	
 }
